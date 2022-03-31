7d843e14bbd243d5551d4c8d6dca2238376ccb3c
dimasadriyantos@gmail.com <dimasdz99>
2022-03-31 10:02:06 +0700

    test fail

---
+++ src/test/java/dzcorp/myschool/MyschoolApplicationTest.java
@@ -0,0 +1,17 @@
+package dzcorp.myschool;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class MyschoolApplicationTest {
+
+    @Test
+    void contextLoads() {
+        Assertions.fail("OOOPPSSSSSS TEST FAIL DIMAS !!!!!!!!!!");
+    }
+
+
+}
