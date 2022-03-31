c5866e24af5911c9600fd1efc0a9c80ad901ef97
dimasadriyantos@gmail.com <dimasdz99>
2022-03-31 11:19:12 +0700

    new update

---
+++ src/test/java/dzcorp/myschool/MyschoolApplicationTest.java
@@ -0,0 +1,18 @@
+package dzcorp.myschool;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MyschoolApplicationTest {
+
+
+    @Test
+    void itShouldFail() {
+        Assertions.fail("SHOULD BE FAIL ======");
+    }
+
+}
